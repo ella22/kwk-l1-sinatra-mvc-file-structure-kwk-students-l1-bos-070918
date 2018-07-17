dcd75bf5b0b1ed70fa6dafd267732b569cdf9aa1
Ella Sklar <ella.soccer22@gmail.com>
2018-07-17 15:17:48 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,7 +1,20 @@
 class Dog
+ attr_accessor :name, :breed, :age 
+ 
+ @@dog_all = []
+ 
+ 
+ 
+def initialize(name,breed,age)
+@name = name 
+@breed = breed
+@age = age
+@dog_all << self 
+end 
 
+def self.all
+@@dog_all
+end
 
+end 
 
-
-
-end   
